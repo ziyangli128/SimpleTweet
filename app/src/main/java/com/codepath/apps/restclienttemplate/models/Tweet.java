35610ba1bbd0aa46eaf722f99f5d5faa270b03a7
Ziyang Li <lzy@fb.com>
2020-06-27 12:13:14 -0700

    Initial commit. Creating Timeline Activity and Row Layout.

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,36 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tweet {
+
+    // some fields for a Tweet object
+    public String body;
+    public String createdAt;
+    public User user;
+
+    // turn a JsonObject into a java Tweet Object
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("test");
+        tweet.createdAt = jsonObject.getString("create_at");
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+
+    // turn a JsonArray into a list of Tweet objects
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            tweets.add(fromJson((jsonArray.getJSONObject(i))));
+        }
+        return tweets;
+    }
+}
